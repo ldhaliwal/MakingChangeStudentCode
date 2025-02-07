c3643f2e59897e9f094502a2f832737f23f1ff75
ldhaliwal <liliana.dhaliwal@menloschool.org>
2025-02-06 18:10:21 -0800

    tabulation √

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -13,10 +13,32 @@ public class MakingChange {
 
     public static long countWays(int target, int[] coins) {
 
-        Arrays.sort(coins);
-        counts = new long[coins.length][target + 1];
+        // tabulation!
+        long[][] counts = new long[coins.length][target + 1];
 
-        return count(target, 0, coins);
+        for (int i = 0; i < coins.length; i++){
+            counts[i][0] = 1;
+        }
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= target; j++) {
+
+                if ((j - coins[i]) >= 0) {
+                    counts[i][j] += counts[i][j - coins[i]];
+                }
+
+                if ((i - 1) >= 0) {
+                    counts[i][j] += counts[i - 1][j];
+                }
+            }
+        }
+        return counts[coins.length - 1][target];
+
+//         Un-comment for memoization:
+//        Arrays.sort(coins);
+//        counts = new long[coins.length][target + 1];
+//
+//        return count(target, 0, coins);
     }
 
     public static long count(int target, int index, int[] coins) {
